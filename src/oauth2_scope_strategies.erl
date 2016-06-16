18cec8654916551b30caaf5f003cdd38cb1669a5
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2016-06-16 22:50:20 +0200

    app: set_subpath() addition
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- src/oauth2_scope_strategies.erl
+++ src/oauth2_scope_strategies.erl
@@ -1,12 +1,22 @@
 -module(oauth2_scope_strategies).
 
 %% API exports
--export([]).
+-export([set_subpath/2, set_subpath/3]).
+
+-type single_scope()  :: binary().
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
+-spec set_subpath(single_scope(), single_scope()) -> single_scope().
+set_subpath(BaseScope, ExtendedScope) ->
+  << BaseScope/binary, <<".">>/binary, ExtendedScope/binary >>.
+
+-spec set_subpath(single_scope(), single_scope(), single_scope()) ->
+    single_scope().
+set_subpath(BaseScope, FirstLevel, SecondLevel) ->
+  set_subpath(set_subpath(BaseScope, FirstLevel), SecondLevel).
 
 %%====================================================================
 %% Internal functions
